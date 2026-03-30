a80b1b4081cf3954a69164f03fa71961a827699e
Qian Chen <elgs1980@hotmail.com>
2026-03-30 00:18:52 -0700

    Update ampere to v0.0.21

--- Casks/ampere.rb
+++ Casks/ampere.rb
@@ -1,6 +1,6 @@
 cask "ampere" do
-  version "0.0.20"
-  sha256 "0a2325a2ed1a782d31dc33d357bb7812d3375544607f02e6e658d35e87ac6f15"
+  version "0.0.21"
+  sha256 "6236aac88178a813ac0811e2c4cd8a7225b78d06f3adc9aa68ff213c50951542"
 
   url "https://github.com/elgs/ampere/releases/download/v#{version}/Ampere.dmg"
   name "Ampere"
