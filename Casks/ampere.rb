cf43c2949ba9b941f90670828ad0a77521e86d3d
Qian Chen <elgs1980@hotmail.com>
2026-07-14 19:40:24 -0700

    Update ampere to v0.0.53

--- Casks/ampere.rb
+++ Casks/ampere.rb
@@ -1,6 +1,6 @@
 cask "ampere" do
-  version "0.0.52"
-  sha256 "4f16be5acb6c5d59ad98be09d33c6d5c4c1b2909de2cada2066355a1cc4b4d4d"
+  version "0.0.53"
+  sha256 "e6f8a310661b6d02c77248a0c90482af84f76ab956746f142afdd79a22dd80b9"
 
   url "https://github.com/az-code-lab/ampere/releases/download/v#{version}/Ampere.dmg"
   name "Ampere"
